5a4cf8a457365250dc392ee4c19444b0b7aa31ba
LudwigEffort <dr.feynman65@gmail.com>
2023-11-18 18:38:41 +0100

    add table

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -39,8 +39,38 @@ getBtn.addEventListener("click", async (event) => {
     const result = await labAPI.get(token);
     console.log(token);
     console.log(result);
+    generateTable(result);
   } catch (error) {
     console.error("Failed to get: " + error);
     console.log(token);
   }
 });
+
+function generateTable(data) {
+  var tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = ""; // Pulisce la tabella prima di popolarla
+  data.forEach(function (item) {
+    var row = `<tr>
+        <td>${item.id}</td>
+        <td>${item.itemName}</td>
+        <td>${item.itemType}</td>
+        <td>${item.description}</td>
+        <td>${item.techSpec}</td>
+        <td>${item.itemIdentifier}</td>
+        <td>${item.status}</td>
+        <td>${new Date(item.creationDate).toLocaleDateString()}</td>
+        <td>${item.roomId}</td>
+        <td>${
+          item.softwares.map((software) => software.softwareName).join(", ") ||
+          "Nessun Software"
+        }</td>
+        <td><button class="btn btn-primary btn-sm" onclick="editItem(${
+          item.id
+        })">Edit</button></td>
+        <td><button class="btn btn-danger btn-sm" onclick="deleteItem(${
+          item.id
+        })">Delite</button></td>
+    </tr>`;
+    tableBody.innerHTML += row;
+  });
+}
