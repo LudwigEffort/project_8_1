a4f77beb26148b9de2b8111f16782303840f520f
LudwigEffort <dr.feynman65@gmail.com>
2023-11-19 23:52:30 +0100

    add PUT in lab user frontend

--- Frontend/js/labUser.js
+++ Frontend/js/labUser.js
@@ -169,3 +169,51 @@ createForm.addEventListener("submit", async (event) => {
 createItemButton.addEventListener("click", () => {
   document.getElementById("createContainer").style.display = "block";
 });
+
+//?? PUT
+async function loadItemById(id, token) {
+  try {
+    const restAPI = new RestAPI("http://localhost:5005/LabManager/labUser");
+    const data = await restAPI.getById(id, token);
+    document.getElementById("editId").value = data.id;
+    document.getElementById("editFirstName").value = data.firstName;
+    document.getElementById("editLastName").value = data.lastName;
+    document.getElementById("editEmail").value = data.emailAddress;
+    document.getElementById("editPhone").value = data.phoneNumber;
+    document.getElementById("editRole").value = data.role;
+    document.getElementById("editToken").value = data.token;
+  } catch (error) {
+    console.error("Failed to load data: " + error);
+  }
+}
+
+const updateForm = document.getElementById("editForm");
+
+updateForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  try {
+    const itemId = document.getElementById("editId").value;
+    const itemData = {
+      id: parseInt(itemId, 10),
+      firstName: document.getElementById("editFirstName").value,
+      lastName: document.getElementById("editLastName").value,
+      emailAddress: document.getElementById("editEmail").value,
+      phoneNumber: document.getElementById("editPhone").value,
+      role: document.getElementById("editRole").value,
+      token: document.getElementById("editToken").value,
+    };
+
+    const restAPI = new RestAPI("http://localhost:5005/LabManager/labUser");
+
+    const reusult = await restAPI.update(itemId, itemData, token);
+    console.log(reusult);
+  } catch (error) {
+    console.error("Failed to update data: " + error);
+  }
+});
+
+function editItem(id) {
+  loadItemById(id, token).then(() => {
+    document.getElementById("editContainer").style.display = "block";
+  });
+}
